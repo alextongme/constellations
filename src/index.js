0a36ccac027b75e1038ebbe67c93122cc77fba06
Alex Tong <tongsalex@gmail.com>
2020-06-21 22:04:39 -0400

    started tutorial for three.js, cube is rendering

--- src/index.js
+++ src/index.js
@@ -1,55 +1,68 @@
-import "./styles/index.scss";
+import "./styles/reset.css";
+import "./styles/index.css";
 import canvasExample from "./scripts/canvas";
-// const testObj = {
-//   key1: "hi",
-//   key2: {
-//     key3: "Hello",
-//   },
-// };
-
-// const greeting = testObj?.key2?.key3 || testObj.key1;
-// window.addEventListener("DOMContentLoaded", () => {
-//   document.body.classList.add("center");
-//   const card = document.createElement("div");
-//   card.classList.add("card", "center");
-//   card.innerHTML = `<h2>${greeting} World!</h2>`;
-//   document.body.append(card);
-//   const imgCard = document.createElement("div");
-//   imgCard.classList.add("card", "center", "image-card");
-//   document.body.appendChild(imgCard);
-// });
 
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  const canvas = new canvasExample();
-  canvas.createCanvas();
 
-  let animating = true;
+// three implementation
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  const animation = () => {
-    canvas.clearSquare();
-    if (animating) canvas.updateSquare();
-    canvas.drawSquare();
-    window.requestAnimationFrame(animation);
-    if (canvas.coords[0] + canvas.coords[2] > canvas.canvas.width)
-      canvas.reverseAnimation();
-    if (canvas.coords[0] < 0) canvas.reverseAnimation();
-  };
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-  window.requestAnimationFrame(animation);
+// lets add a cube in
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-  window.addEventListener("keydown", (event) => {
-    if (event.which === 32) {
-      event.preventDefault();
-      canvas.reverseAnimation();
-      canvas.setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
-    }
-  });
+camera.position.z = 5;
 
-  window.addEventListener("mousedown", (event) => {
-    event.preventDefault();
-    console.log("click");
-    animating = !animating;
-  });
+// render our cube
+function animate() {
+  requestAnimationFrame( animate );
+  cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+animate();
+
+// old code
+  // const canvas = new canvasExample();
+  // canvas.createCanvas();
+
+  // let animating = true;
+
+  // const animation = () => {
+  //   canvas.clearSquare();
+  //   if (animating) canvas.updateSquare();
+  //   canvas.drawSquare();
+  //   window.requestAnimationFrame(animation);
+  //   if (canvas.coords[0] + canvas.coords[2] > canvas.canvas.width)
+  //     canvas.reverseAnimation();
+  //   if (canvas.coords[0] < 0) canvas.reverseAnimation();
+  // };
+
+  // window.requestAnimationFrame(animation);
+
+  // window.addEventListener("keydown", (event) => {
+  //   if (event.which === 32) {
+  //     event.preventDefault();
+  //     canvas.reverseAnimation();
+  //     canvas.setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+  //   }
+  // });
+
+  // window.addEventListener("mousedown", (event) => {
+  //   event.preventDefault();
+  //   console.log("click");
+  //   animating = !animating;
+  // });
+
+
 }
